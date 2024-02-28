01323b0068012d3607c0adc78bbad8c5d2d3453f
Kevin Martins <kev.martinsvieira@gmail.com>
2024-02-29 00:32:16 +0100

    fix: change svg height props to length

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,9 +13,9 @@ const NavBar = () => {
 
   return (
     <nav className="fixed z-50 left-[50%] top-8 flex w-fit -translate-x-[50%] items-center gap-6 rounded-lg border-[1px] border-neutral-700 hover:border-neutral-500 bg-neutral-900 p-2 text-sm text-neutral-500">
-      <svg width="24" height="auto" viewBox="0 0 50 39" fill="none" xmlns="http://www.w3.org/2000/svg" className="ml-2 fill-neutral-50">
-        <path d="M16.4992 2H37.5808L22.0816 24.9729H1L16.4992 2Z" stop-color="#000000" />
-        <path d="M17.4224 27.102L11.4192 36H33.5008L49 13.0271H32.7024L23.2064 27.102H17.4224Z" stop-color="#000000" />
+      <svg width="24" length="auto" viewBox="0 0 50 39" fill="none" xmlns="http://www.w3.org/2000/svg" className="ml-2 fill-neutral-50">
+        <path d="M16.4992 2H37.5808L22.0816 24.9729H1L16.4992 2Z" stopColor="#000000" />
+        <path d="M17.4224 27.102L11.4192 36H33.5008L49 13.0271H32.7024L23.2064 27.102H17.4224Z" stopColor="#000000" />
       </svg>
       <a href="#" rel="nofollow" className="block overflow-hidden">
         <div className="h-[20px]" style={{ transform: "none" }}>

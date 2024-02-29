96f82c3b2786257f7a8eccec01067d0ecd17cbfd
Kevin Martins <kev.martinsvieira@gmail.com>
2024-03-01 00:10:38 +0100

    feat: modify business-fields components

--- src/components/ScrollContent.jsx
+++ src/components/ScrollContent.jsx
@@ -2,9 +2,9 @@ import { useScroll, useTransform } from 'framer-motion';
 import React from 'react'
 
 const ImageContent = ({ imageUrl, imageAlt, i }) => (
-  <div className='lg:w-1/2'>
+  <div className='lg:w-1/2 h-full'>
     <img
-      className={`h-56 object-cover sm:h-72 md:h-96 lg:w-full lg:h-full ${i % 2 === 0 ? 'picture-deformation-l rounded-r-lg' : 'picture-deformation-r rounded-l-lg'} `}
+      className={`object-cover ${i % 2 === 0 ? 'picture-deformation-r rounded-r-lg' : 'picture-deformation-l rounded-l-lg'} `}
       src={imageUrl}
       alt={imageAlt}
     />
@@ -15,80 +15,77 @@ const ScrollContent = ({ field, i }) => {
   const { scrollYProgress } = useScroll()
   const scale = useTransform(scrollYProgress, [0, 1], [0.2, 2]);
   return (
-    <div className='relative overflow-hidden my-16'>
-      <div className='max-w-7xl mx-auto flex flex-col lg:flex-row max-w-7xl mx-auto'>
-        {i % 2 !== 0 && <ImageContent imageUrl={field.image} imageAlt={field.alt} i={i} />}
-        <div className='relative z-10 pb-8 sm:pb-16 md:pb-20 lg:w-1/2'>
-          {/* text */}
-          <div className='relative pt-6 sm:px-6 lg:px-8'>
-            <div className='mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28'>
-              <div className='sm:text-center lg:text-left'>
-                <h1 className='text-4xl tracking-tight font-extrabold text-slate-100 sm:text-5xl xl:text-6xl'>
-                  <span className='block xl:inline'>
-                    {field.title}
-                    {' '}
-                  </span>
-                  <span className='block text-yellow-500 xl:inline'>
-                    {field.highlight}
-                  </span>
-                </h1>
-                <p className='mt-3 text-base text-slate-400 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 xl:text-xl lg:mx-0'>
-                  {field.description}
-                </p>
-                <div className='mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start gap-3'>
-                  <a
-                    href={field.photo}
-                    className="px-6 py-4 relative z-0 flex items-center gap-2 overflow-hidden whitespace-nowrap rounded-lg border-[1px] 
-                      border-neutral-500 px-4 py-1.5 font-medium
-                      text-slate-100 transition-all duration-300
-                      before:absolute before:inset-0
-                      before:-z-10 before:translate-y-[200%]
-                      before:scale-[2.5]
-                      before:rounded-[100%] before:bg-yellow-600
-                      before:transition-transform before:duration-1000
-                      before:content-[&quot;&quot;]
-                      hover:scale-105 hover:border-yellow-600 hover:text-slate-300
-                      hover:before:translate-y-[0%]
-                      active:scale-100"
-                  >
-                    voir les photos
-                  </a>
-                  <a
-                    href={field.photo}
-                    className="px-6 py-4 relative z-0 flex items-center gap-2 overflow-hidden whitespace-nowrap rounded-lg border-[1px] 
-                      border-yellow-600 px-4 py-1.5 font-medium
-                      text-slate-300 transition-all duration-300
-                      before:absolute before:inset-0
-                      before:-z-10 before:translate-y-[0%]
-                      before:scale-[2.5]
-                      before:rounded-[100%] before:bg-yellow-600
-                      before:transition-transform before:duration-1000
-                      before:content-[&quot;&quot;]
-                      hover:scale-105 hover:border-neutral-500 hover:text-slate-100
-                      hover:before:translate-y-[-200%]
-                      active:scale-100"
-                  >
-                    prendre contact
-                  </a>
-                  {/* <a
-                    href={field.photo}
-                    className='w-content flex items-center justify-center px-8 py-3 text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700 dark:bg-yellow-600 dark:hover:bg-yellow-700 md:py-4 xl:text-lg md:px-10'
-                  >
-                    {field.name}
-                  </a> */}
-                  {/* <a
-                    href={field.contact}
-                    className='w-content flex items-center justify-center px-8 py-3 text-base font-medium rounded-md text-green-700 dark:text-yellow-700 bg-green-100 hover:bg-green-200 dark:bg-yellow-100 dark:hover:bg-yellow-200 md:py-4 xl:text-lg md:px-10'
-                  >
-                    {field.name}
-                  </a> */}
-                </div>
-              </div>
-            </div>
+    <div className='relative overflow-hidden max-w-6xl mx-auto flex flex-col lg:flex-row my-20'>
+      {i % 2 !== 0 && <ImageContent imageUrl={field.image} imageAlt={field.alt} i={i} />}
+      <div className='relative z-10 lg:w-1/2 '>{/* pb-8 sm:pb-16 md:pb-20 */}
+        {/* text */}
+        {/* <div className='relative pt-6 sm:px-6 lg:px-8'>
+          <div className='mt-10 mx-auto px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28'>
+            <div className='sm:text-center lg:text-left '> */}
+        <div className='flex flex-col h-full justify-center items-center text-center md:text-left'>
+          <h1 className='text-4xl tracking-tight font-extrabold text-slate-100 sm:text-5xl xl:text-6xl'>
+            <span className='block xl:inline'>
+              {field.title}
+              {' '}
+            </span>
+            <span className='block text-yellow-500 xl:inline'>
+              {field.highlight}
+            </span>
+          </h1>
+          <p className='mt-3 text-base text-slate-400 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 xl:text-xl lg:mx-0'>
+            {field.description}
+          </p>
+          <div className='mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start gap-3'>
+            <a
+              href={field.photo}
+              className="px-7 py-4 relative z-0 flex items-center gap-2 overflow-hidden whitespace-nowrap rounded-lg border-[1px] 
+                border-neutral-500 font-medium
+                text-slate-100 transition-all duration-300
+                before:absolute before:inset-0
+                before:-z-10 before:translate-y-[200%]
+                before:scale-[2.5]
+                before:rounded-[100%] before:bg-yellow-600
+                before:transition-transform before:duration-1000
+                before:content-[&quot;&quot;]
+                hover:scale-105 hover:border-yellow-600 hover:text-slate-300
+                hover:before:translate-y-[0%]
+                active:scale-100"
+            >
+              voir les photos
+            </a>
+            <a
+              href={field.photo}
+              className="px-7 py-4 relative z-0 flex items-center gap-2 overflow-hidden whitespace-nowrap rounded-lg border-[1px] 
+                border-yellow-600 font-medium
+                text-slate-300 transition-all duration-300
+                before:absolute before:inset-0
+                before:-z-10 before:translate-y-[0%]
+                before:scale-[2.5]
+                before:rounded-[100%] before:bg-yellow-600
+                before:transition-transform before:duration-1000
+                before:content-[&quot;&quot;]
+                hover:scale-105 hover:border-neutral-500 hover:text-slate-100
+                hover:before:translate-y-[-200%]
+                active:scale-100"
+            >
+              prendre contact
+            </a>
+            {/* <a
+              href={field.photo}
+              className='w-content flex items-center justify-center px-8 py-3 text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700 dark:bg-yellow-600 dark:hover:bg-yellow-700 md:py-4 xl:text-lg md:px-10'
+            >
+              {field.name}
+            </a> */}
+            {/* <a
+              href={field.contact}
+              className='w-content flex items-center justify-center px-8 py-3 text-base font-medium rounded-md text-green-700 dark:text-yellow-700 bg-green-100 hover:bg-green-200 dark:bg-yellow-100 dark:hover:bg-yellow-200 md:py-4 xl:text-lg md:px-10'
+            >
+              {field.name}
+            </a> */}
           </div>
         </div>
-        {i % 2 === 0 && <ImageContent imageUrl={field.image} imageAlt={field.alt} i={i} />}
       </div>
+      {i % 2 === 0 && <ImageContent imageUrl={field.image} imageAlt={field.alt} i={i} />}
     </div >
   )
 }
--- src/constants/business-fields.js
+++ src/constants/business-fields.js
@@ -4,7 +4,7 @@ export const businessFields = [
     highlight: "un Espace Convivial",// un Espace Lumineux et Convivial
     name: 'véranda',
     description: "Transformez votre espace extérieur en un lieu lumineux et confortable. Profitez de la nature dans un cadre abrité et accueillant.",
-    image: '/assets/veranda.png',
+    image: '/assets/veranda_1.png',
     alt: "une véranda",
     photos: '/realisations/verandas',
     contact: '/contact#verandas',
@@ -14,7 +14,7 @@ export const businessFields = [
     highlight: 'Illuminer votre Intérieur',
     name: "fenêtre",
     description: "Fenêtres élégantes pour illuminer votre intérieur. Alliez esthétique et fonctionnalité avec nos solutions de fenêtres sur mesure.",
-    image: '/assets/fenetre.png',
+    image: '/assets/fenetre_1.png',
     alt: "une fenêtre",
     photos: '/realisations/fenetres',
     contact: '/contact#fenetre',
@@ -24,7 +24,7 @@ export const businessFields = [
     highlight: "Sécuriser votre Habitat",
     name: 'alarme et domotique',
     description: "Sécurité avancée et confort connecté. Surveillez et contrôlez votre domicile où que vous soyez, pour une tranquillité d'esprit totale.",
-    image: '/assets/store.png',
+    image: '/assets/veranda_4.png',
     alt: "une installation domotique",
     photos: '/realisations/alarmes-et-domotique',
     contact: '/contact#alarmes-et-domotique',
@@ -34,7 +34,7 @@ export const businessFields = [
     highlight: 'Sublimer vos Espaces',
     name: "store",
     description: "Alliant esthétisme et fonctionnalité, notre gamme de stores vous offre la possibilité de régler la luminosité selon vos préférences.",
-    image: '/assets/garage.png',
+    image: '/assets/porte_garage_1.png',
     alt: 'un store',
     photos: '/realisations/stores',
     contact: '/contact#stores',
@@ -44,7 +44,7 @@ export const businessFields = [
     highlight: 'un Accueil Chaleureux',// une Entrée Accueillante
     name: 'porte et portail',
     description: "Accueillez vos proches avec élégance et assurez à votre foyer la meilleur des protection. Nos portes et portails sur mesure allient sécurité et esthétique.",//  pour créer un accueil qui reflète votre style
-    image: '/assets/portail.png',
+    image: '/assets/portail_1.png',
     alt: "une porte d'entrée",
     photos: '/realisations/portes-et-portails',
     contact: '/contact#portes-et-portails',
@@ -54,7 +54,7 @@ export const businessFields = [
     highlight: 'Assurer votre Confort',// la Sécurité et le Confort, un Confort Assuré
     name: 'volet roulant',
     description: "Contrôlez la lumière et l'obscurité à votre convenance. Nos volets roulants offrent une solution pratique et élégante.",
-    image: '/assets/portail.png',
+    image: '/assets/porte_entree_1.png',
     alt: 'des volets',
     photos: '/realisations/volets-roulants',
     contact: '/contact#volets-roulants',

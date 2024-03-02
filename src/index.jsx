da1e9b873ced158e2c3d9075bdbfe4e625a10da2
kevin-martins <kev.martinsvieira@gmail.com>
2024-03-03 00:11:52 +0100

    feat: add hydrate & render from react-dom, for later use

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,15 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './components/App';
 import Loading from './components/Loading'
+import { hydrate, render } from "react-dom";
 import './styles/index.css';
 
 const container = document.getElementById('root');
+// if (rootElement.hasChildNodes()) {
+//   hydrate(<App />, rootElement);
+// } else {
+//   render(<App />, rootElement);
+// }
 const root = createRoot(container);
 
 root.render(

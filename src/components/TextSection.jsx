b340bb9e6e5c490fbe5974137816f012078f9d37
Kevin Martins <kev.martinsvieira@gmail.com>
2024-03-15 21:55:45 +0100

    feat: add id to textSection and remove scrollBar view

--- src/components/TextSection.jsx
+++ src/components/TextSection.jsx
@@ -1,12 +1,13 @@
 import React, { useRef } from 'react'
 import { motion, useInView } from 'framer-motion'
 
-export const TextSection = ({ title, text }) => {
+export const TextSection = ({ id, title, text }) => {
   const ref = useRef(null)
   const isInView = useInView(ref, { once: false })
   return (
     <motion.section
       ref={ref}
+      id={id}
       style={{
         transform: isInView ? "none" : "translateY(100px)",
         opacity: isInView ? 1 : 0,
@@ -14,9 +15,9 @@ export const TextSection = ({ title, text }) => {
       }}
       className='w-full mt-12 pb-10'
     >
-      <h1 className='text-3xl md:text-5xl w-max mx-auto skew-x-12'>
+      <h2 className='text-3xl md:text-5xl w-max mx-auto skew-x-12'>
         {title}
-      </h1>
+      </h2>
       <div className='bg-yellow-600 w-24 md:w-32 mt-3 m-auto h-[.3px]' />
       <p className='text-slate-400 mt-8 md:max-w-5xl px-12 text-sm md:text-lg mx-auto text-justify'>
         {text}

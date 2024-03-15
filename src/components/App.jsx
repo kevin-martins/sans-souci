5fc7f505d15cf1b572f239d65cd6d8496aab6e2a
Kevin Martins <kev.martinsvieira@gmail.com>
2024-03-15 17:18:18 +0100

    add scroll Effect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,39 @@
-import React, { useState } from 'react';
-import { HashRouter, Route, Routes } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { addNotification } from '../features/dataSlice';
-import { useSelector } from 'react-redux';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NavBar from './NavBar';
 import Home from '../pages/Home';
 import PictureGallery from '../pages/PictureGallery';
 import Error404 from '../pages/Error404';
+import { HelmetProvider } from 'react-helmet-async';
+import { motion, useScroll, useSpring } from 'framer-motion';
 
 export const App = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const notifications = useSelector(state => state?.data.notifications);
-  const dispatch = useDispatch();
-
-  const onClick = () => {
-    // setIsOpen(prev => !prev);
-    const randomIndex = Math.floor(Math.random() * 10000);
-    const notification = {
-      id: randomIndex,
-      icon: <></>,
-      text: "notification"
-    }
-    dispatch(addNotification(notification));
-  }
+  const { scrollYProgress } = useScroll()
+  const scaleY = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  })
 
   return (
-    <>
+    <HelmetProvider>
       <main className='text-slate-100 bg-gradient-to-t from-slate-900 to-slate-950'>
-          <HashRouter>
+          <BrowserRouter>
             <NavBar />
-              <div className='min-h-screen'>
-                <div className='container overflow-x-hidden mx-auto'>
-                  <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="galerie-photo" element={<PictureGallery />} />
-                    <Route path="*" element={<Error404 />} />
-                  </Routes>
-                </div>
+            <div className='min-h-screen'>
+              <div className='container overflow-x-hidden mx-auto'>
+                <div className="fixed right-0 top-px w-1 h-screen bg-slate-700 origin-top" />
+                <motion.div className="fixed right-0 top-px w-1 h-screen bg-slate-200 origin-top" style={{ scaleY }} />
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="galerie-photo" element={<PictureGallery />} />
+                  <Route path="*" element={<Error404 />} />
+                </Routes>
               </div>
-          </HashRouter>
+            </div>
+          </BrowserRouter>
         </main>
-    </>
+    </HelmetProvider>
   );
 }
 
